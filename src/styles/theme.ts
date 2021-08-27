86db8fde3a550277b61fb571c5e9df772838388d
mauriciobrito7 <maubot01@gmail.com>
2021-08-26 22:51:10 -0400

    theme and utils for styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,65 @@
+export const colors = {
+  white: "#ffffff",
+  background: '#fcfcfc',
+  blackTitle: "#3b3b3b",
+  blackParagraph: '#606060',
+  blackItemText: '#343434',
+  hoverItem: '#eaeaea',
+  border: '#979797',
+  primary: '#1797ff',
+};
+
+export const gutter = '14px';
+export const borderRadius = '4px';
+
+export const fontConfig = {
+  font: {
+    body: 'Roboto',
+    headers: 'Baskervville'
+  },
+  fontSize: {
+    body: {
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '14px',
+      lineHeight: '14px',
+    },
+    caption: {
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: '14px',
+      lineHeight: '12px',
+    },
+    h1: {
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '28px',
+      lineHeight: '33px',
+    },
+    h2: {
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '24px',
+      lineHeight: '28px',
+    },
+    h3: {
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '11px',
+      lineHeight: '13px',
+    },
+    button: {
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '14px',
+      lineHeight: '16px',
+    },
+  },
+};
+
+export const breakpoints = {
+  mobile: "375px",
+  tablet: "768px",
+  desktop: "1200px",
+  large: "1400px",
+};
