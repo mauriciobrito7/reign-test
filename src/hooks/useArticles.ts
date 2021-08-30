b2e1fd8b5fb46822daacf1c991fb17413dfc434d
mauriciobrito7 <maubot01@gmail.com>
2021-08-30 02:14:24 -0400

    hook for articles

---
+++ src/hooks/useArticles.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { fetchArticles } from '../utils/api';
+
+export default function useArticles() {
+  const [articles, setArticles] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getArticles = async () => {
+    try {
+      await fetchArticles().then((data) => setArticles(data));
+    } catch (error) {
+      console.log(`Error ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getArticles();
+    setIsLoading(false);
+  }, []);
+
+  return [articles, isLoading];
+};
